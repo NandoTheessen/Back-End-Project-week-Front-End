df352e1b3617b91dd0ef761124f9a797bf1e4a5f
NandoTheessen <nando.theessen@gmail.com>
2018-06-28 17:48:11 +0200

    Small fix

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -6,7 +6,7 @@ const SideBar = (props) => {
         <div className='sidebar'>
             <h1>Easy Notes</h1>
             <p>Welcome Back!</p>
-            <Button text='+' class='addNote' function={this.props.history.push('/createNote')} />
+            <Button text='+' class='addNote' function={() => this.props.history.push('/createNote')} />
             <Button text='LogOut' function={props.logout} />
         </div>
     );
