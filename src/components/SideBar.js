fde82e2555a211218e74fb58624478292aa642bc
NandoTheessen <nando.theessen@gmail.com>
2018-06-28 18:13:17 +0200

    small fixes for the sidebar

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Button from './Button';
+import { Link } from 'react-router';
 
 const SideBar = (props) => {
     return (
         <div className='sidebar'>
             <h1>Easy Notes</h1>
             <p>Welcome Back!</p>
-            <Button text='+' class='addNote' function={() => this.props.history.push('/createNote')} />
+            <Link to='/createNote'><Button text='+' class='addNote' /></Link>
             <Button text='LogOut' function={props.logout} />
         </div>
     );
