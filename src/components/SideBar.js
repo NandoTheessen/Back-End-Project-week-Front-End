20892fa2f274fb40229a6c3d20f91af3c5cad0f7
NandoTheessen <nando.theessen@gmail.com>
2018-06-28 18:16:29 +0200

    small fixes for the sidebar

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Button from './Button';
-import { Link } from 'react-router';
+import { Link } from 'react-router-dom';
 
 const SideBar = (props) => {
     return (
