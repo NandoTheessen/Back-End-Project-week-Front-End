03baa93565f62b7223e721cbc75a459b78756349
NandoTheessen <nando.theessen@gmail.com>
2018-07-05 13:13:17 +0200

    changes to button placement

--- src/components/Content.js
+++ src/components/Content.js
@@ -14,6 +14,8 @@ const Content = (props) => {
             <Route path='/note' render={x => <Sidebar {...x} loggedin={props.loggedin} logout={props.logout} />} />
             <Route exact path='/note/create' render={x => <CreateNote {...x} page='create' function={props.saveNote} />} />
             <Route exact path='/note/edit' render={x => <CreateNote {...x} page='update' note={props.note} function={this.updateNote} />} />
+            <button className='addNote-below750' onClick={() => props.history.push('/note/create')}>+</button>
+            <button className='logout-below750' onClick={props.logout} ><i className="fas fa-door-open"></i></button>
         </div>
     );
 };
